158e69257980b4b9df48e554e71d6b1f09b668f4
mswann11 <mswann11@gmail.com>
2018-03-13 19:54:08 -0400

    initial commit

---
+++ app/src/main/java/com/example/android/diabetes/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.android.diabetes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/**
+ * This app
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
