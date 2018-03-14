b8496dbc13adb075336101047245e1cbb4e91e3d
mswann11 <mswann11@gmail.com>
2018-03-14 12:22:30 -0400

    Add basic functionality for each page.

--- app/src/main/java/com/example/android/diabetes/Food.java
+++ app/src/main/java/com/example/android/diabetes/Food.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class Food extends AppCompatActivity {
 
@@ -11,6 +14,24 @@ public class Food extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food);
+
+        Spinner vegetableSpinner = (Spinner) findViewById(R.id.vegetable_spinner);
+        ArrayAdapter<CharSequence> vegetableAdapter = ArrayAdapter.createFromResource(this,
+                R.array.vegetable_array, R.layout.spinner_item);
+        vegetableAdapter.setDropDownViewResource(R.layout.spinner_dropdown_item);
+        vegetableSpinner.setAdapter(vegetableAdapter);
+
+        Spinner proteinSpinner = (Spinner) findViewById(R.id.protein_spinner);
+        ArrayAdapter<CharSequence> proteinAdapter = ArrayAdapter.createFromResource(this,
+                R.array.protein_array, R.layout.spinner_item);
+        proteinAdapter.setDropDownViewResource(R.layout.spinner_dropdown_item);
+        proteinSpinner.setAdapter(proteinAdapter);
+
+        Spinner grainSpinner = (Spinner) findViewById(R.id.grain_spinner);
+        ArrayAdapter<CharSequence> grainAdapter = ArrayAdapter.createFromResource(this,
+                R.array.grain_array, R.layout.spinner_item);
+        grainAdapter.setDropDownViewResource(R.layout.spinner_dropdown_item);
+        grainSpinner.setAdapter(grainAdapter);
     }
 
     public void main(View view) {
--- app/src/main/java/com/example/android/diabetes/Screening.java
+++ app/src/main/java/com/example/android/diabetes/Screening.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class Screening extends AppCompatActivity {
 
@@ -11,10 +14,34 @@ public class Screening extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screening);
+        Spinner ageSpinner = (Spinner) findViewById(R.id.age_spinner);
+        ArrayAdapter<CharSequence> ageAdapter = ArrayAdapter.createFromResource(this,
+                R.array.age_array, android.R.layout.simple_spinner_item);
+        ageAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        ageSpinner.setAdapter(ageAdapter);
+
+        Spinner sexSpinner = (Spinner) findViewById(R.id.sex_spinner);
+        ArrayAdapter<CharSequence> sexAdapter = ArrayAdapter.createFromResource(this,
+                R.array.sex_array, android.R.layout.simple_spinner_item);
+        sexAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sexSpinner.setAdapter(sexAdapter);
+
+        Spinner gestationalSpinner = (Spinner) findViewById(R.id.gestational_spinner);
+        ArrayAdapter<CharSequence> gestationalAdapter = ArrayAdapter.createFromResource(this,
+                R.array.gestational_array, android.R.layout.simple_spinner_item);
+        gestationalAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        gestationalSpinner.setAdapter(gestationalAdapter);
+
+        Spinner relativeSpinner = (Spinner) findViewById(R.id.relative_spinner);
+        ArrayAdapter<CharSequence> relativeAdapter = ArrayAdapter.createFromResource(this,
+                R.array.relative_array, android.R.layout.simple_spinner_item);
+        relativeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        relativeSpinner.setAdapter(relativeAdapter);
     }
 
     public void main(View view) {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
+
 }
